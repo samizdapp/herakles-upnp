eb9c90d26a22b12b973372ea450c91b0822db029
Ryan Bennett <nomad.ry@gmail.com>
2022-06-22 03:53:39 -0600

    port change

--- update.sh
+++ update.sh
@@ -5,6 +5,8 @@ do
     WAN_ADDR=$(dig +short txt ch whoami.cloudflare @1.0.0.1 | tr -d '"')
     LAN_ADDR=$(upnpc -l | grep "Local LAN ip address" | cut -d: -f2)
     echo "[\"$WAN_ADDR:8889\",\"$LAN_ADDR\"]" > /upnp/addresses
-    upnpc -r 80 8889 TCP
+    upnpc -r 4000 4000 TCP
+    upnpc -r 4001 4001 TCP
+    upnpc -r 80 9090 TCP
 	sleep 3600
 done
