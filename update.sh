29b028fdb9b2bcd04e009e1011d60372a0df9b2f
Ryan Bennett <nomad.ry@gmail.com>
2022-08-01 21:23:09 -0600

    increment port for WAN

--- update.sh
+++ update.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
+
+# if [ ! -f ./port ]
+# then
+echo "4000" > /upnp/port
+# fi
+
+
 while :
 do
 	echo "Press [CTRL+C] to stop.."
+    PORT=$(cat /upnp/port)
     WAN_ADDR=$(dig +short txt ch whoami.cloudflare @1.0.0.1 | tr -d '"')
     LAN_ADDR=$(upnpc -l | grep "Local LAN ip address" | cut -d: -f2)
-    echo "[\"$WAN_ADDR:8889\",\"$LAN_ADDR\"]" > /upnp/addresses
-    upnpc -r 4000 4000 TCP
-    upnpc -r 4001 4001 TCP
-    upnpc -r 80 9090 TCP
+    echo "try port $PORT"
+    while ! upnpc -r 4000 $PORT TCP
+    do
+        PORT=$(($PORT + 1))
+        echo "increment port $PORT"
+        echo $PORT > /upnp/port
+    done
+
+
+    echo "[\"$WAN_ADDR:$PORT\",\"$LAN_ADDR:4000\"]" > /upnp/addresses
+    
 	sleep 3600
 done
